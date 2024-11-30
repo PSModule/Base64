6685c284af5765713afa6133475b08a7b27303ec
Marius Storhaug <marstor@hotmail.com>
2024-11-30 13:49:57 +0100

    Add tests for Base64 encoding and decoding functionality

--- tests/Module.Tests.ps1
+++ tests/Module.Tests.ps1
@@ -4,12 +4,21 @@
             $base64 = New-Base64 -InputString 'ThisIsANiceString'
             $base64.Base64 | Should -Be 'VGhpc0lzQU5pY2VTdHJpbmc='
         }
-        
+
         It 'Encodes and decodes a string' {
             $string = 'Hello, World!'
             $encoded = $string | ConvertTo-Base64
             $decoded = $encoded | ConvertFrom-Base64
             $decoded | Should -Be $string
+        }
+
+        It 'Can test if a string is base64 encoded' {
+            $string = 'Hello, World!'
+            $encoded = $string | ConvertTo-Base64
+            $decoded = $encoded | ConvertFrom-Base64
+            $string | Test-Base64 | Should -Be $false
+            $encoded | Test-Base64 | Should -Be $true
+            $decoded | Test-Base64 | Should -Be $false
         }
     }
 }
